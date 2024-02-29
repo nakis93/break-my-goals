04de773944709823b4da5f12dfd597bda1869e16
nakis93 <zjnakis@gmail.com>
2024-03-01 00:14:45 +0200

    **FIRST COMMIT** *Added angular/auth to project with hosting and authentication *Added tailwind to project *Set up environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    apiKey: "AIzaSyCUJP08cuJpUwn_v--9zCsDVrT8foXGuNg",
+    authDomain: "break-my-goals.firebaseapp.com",
+    projectId: "break-my-goals",
+    storageBucket: "break-my-goals.appspot.com",
+    messagingSenderId: "547471814885",
+    appId: "1:547471814885:web:89a3443dbfac49cce2240c"
+};
+
