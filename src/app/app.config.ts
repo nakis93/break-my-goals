753aabe9909b2e1f307cad186d27935c4034903e
nakis93 <zjnakis@gmail.com>
2024-06-12 10:55:03 +0300

    *Fixed firebase import problem

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,16 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-  importProvidersFrom([
+  providers: [
+    provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment)),
-    provideAuth(() => getAuth())
-  ])]
+    provideAuth(() => getAuth()),
+    provideAnimationsAsync('noop')
+  ]
 };
